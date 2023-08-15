deeb6da282fbda2302ded880a4fab96f61ca74ee
Relg99 <relg1999@gmail.com>
2023-08-14 18:22:42 -0600

    First commit! > > Second Mplab project. > Done with MPLAB PICkit 4. > Microcontroller: PIC16F628A.

---
+++ BCD_01.s
@@ -0,0 +1,38 @@
+
+; PIC16F628A Configuration Bit Settings
+
+; Assembly source line config statements
+
+; CONFIG
+  CONFIG  FOSC = INTOSCIO       ; Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = ON            ; RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+  CONFIG  BOREN = OFF           ; Brown-out Detect Enable bit (BOD disabled)
+  CONFIG  LVP = OFF             ; Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
+  CONFIG  CPD = OFF             ; Data EE Memory Code Protection bit (Data memory code protection off)
+  CONFIG  CP = OFF              ; Flash Program Memory Code Protection bit (Code protection off)
+
+// config statements should precede project file includes.
+  
+#include <xc.inc>
+
+; When assembly code is placed in a psect, it can be manipulated as a
+; whole by the linker and placed in memory.  
+;
+; In this example, barfunc is the program section (psect) name, 'local' means
+; that the section will not be combined with other sections even if they have
+; the same name.  class=CODE means the barfunc must go in the CODE container.
+; PIC18 should have a delta (addressible unit size) of 1 (default) since they
+; are byte addressible.  PIC10/12/16 have a delta of 2 since they are word
+; addressible.  PIC18 should have a reloc (alignment) flag of 2 for any
+; psect which contains executable code.  PIC10/12/16 can use the default
+; reloc value of 1.  Use one of the psects below for the device you use:
+
+psect   barfunc,local,class=CODE,delta=2 ; PIC10/12/16
+; psect   barfunc,local,class=CODE,reloc=2 ; PIC18
+
+global _bar ; extern of bar function goes in the C source file
+_bar:
+    movf PORTA,w    ; here we use a symbol defined via xc.inc
+    return
